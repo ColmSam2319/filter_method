5f92317864350cf3bdf561072858f367483047bf
ColmSam2319 <colmsammon13@gmail.com>
2022-10-25 18:30:59 +0000

    initial commit

--- filter.js
+++ filter.js
@@ -21,6 +21,11 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough)
+
+const onlyPaul = people.filter(person => person.name == 'Paul');
+console.log(onlyPaul)
 
 // Complex Filtering
 const students = [
@@ -55,3 +60,9 @@ const students = [
     ]
   },
 ];
+
+const has5YearsExp = skill => skill.yrsExperience >= 5
+const hasStrongSkills = students => students.skills.filter(has5YearsExp).length > 0;
+const candidates  = students.filter(hasStrongSkills);
+
+console.log(candidates);
